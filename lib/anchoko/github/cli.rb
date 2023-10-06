9f32116466e567b7562eb00e1686759e58b0ee0d
Yuya TAMANO <965378+tamano@users.noreply.github.com>
2023-10-06 17:47:07 +0900

    Implement facade for commit_list command

--- lib/anchoko/github/cli.rb
+++ lib/anchoko/github/cli.rb
@@ -3,6 +3,12 @@ require "thor"
 module Anchoko
   module Github
     class CLI < Thor
+      desc "commit_list PR_LIST_FILE", "Print list of merge commits by PR list"
+      def commit_list(list)
+        # TBD
+        puts "Hello #{list}"
+      end
+
       desc "version", "Print version"
       def version
         puts Anchoko::Github::VERSION
