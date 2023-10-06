051e3152ebe82f37b059321e71e9176fdbc8cef2
Yuya TAMANO <965378+tamano@users.noreply.github.com>
2023-10-06 16:05:26 +0900

    Initial Commit.

---
+++ spec/anchoko/github_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Anchoko::Github do
+  it "has a version number" do
+    expect(Anchoko::Github::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
