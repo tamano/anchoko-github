9e601e9ccbca6c63a0ced36b8f8f91916b9b3090
Yuya TAMANO <965378+tamano@users.noreply.github.com>
2023-10-06 18:00:22 +0900

    Implement validator of commit_list command

--- lib/anchoko/github/cli.rb
+++ lib/anchoko/github/cli.rb
@@ -5,6 +5,16 @@ module Anchoko
     class CLI < Thor
       desc "commit_list PR_LIST_FILE", "Print list of merge commits by PR list"
       def commit_list(list)
+        unless File.exist?(list)
+          puts "File not found: #{list}"
+          exit 1
+        end
+
+        if ENV["GITHUB_TOKEN"].nil?
+          puts "GITHUB_TOKEN is not set"
+          exit 1
+        end
+
         # TBD
         puts "Hello #{list}"
       end
